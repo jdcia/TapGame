2129575aac4cc1ede1de1c8ea382e9cdaaefd444
jdcia <jacob.garcia@student.nmt.edu>
2020-05-04 21:27:08 -0600

    Add buttons and slots for the clicked() event

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -12,3 +12,23 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+//game mode 1 will involve two obstacles.
+void MainWindow::on_game_mode1_clicked()
+{
+
+}
+
+
+//game mode 1 will involve one obstacle
+void MainWindow::on_game_mode2_clicked()
+{
+
+}
+
+
+//will send player to a settings menu
+void MainWindow::on_settings_clicked()
+{
+
+}
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -15,6 +15,13 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_game_mode2_clicked();
+
+    void on_game_mode1_clicked();
+
+    void on_settings_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
